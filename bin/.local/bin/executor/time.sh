d94a446497c30c0064cf0b9da28f749a84046e19
Kenneth Manuel <kennethlieyanto99@gmail.com>
2024-06-14 18:47:52 +0700

    Last update before going back to i3

---
+++ bin/.local/bin/executor/time.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ "$(cat ~/.tw_started_id)" == "" ]; then
+	echo ""
+else
+	project=$(task _get "$(cat ~/.tw_started_id)".project)
+	description=$(task _get "$(cat ~/.tw_started_id)".description)
+
+	if timew >/dev/null 2>&1; then
+		echo "$(timew summary :day "$project" "$description" | awk '{print $NF}' | tail -2 | head -1)"
+	else
+		echo ""
+	fi
+
+	if [ "${1}" = "--toggle" ]; then
+		if timew; then
+			task "$(cat ~/.tw_started_id)" stop
+		else
+			task "$(cat ~/.tw_started_id)" start
+		fi
+	fi
+
+fi
